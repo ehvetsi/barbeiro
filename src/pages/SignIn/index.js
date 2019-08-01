282324839f140e13afdc0da28bf12f354de0da55
Everton Nogueira <everton.nogueira@amcom.com.br>
2019-08-01 10:32:53 -0300

    Adicionando validações

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,7 +1,34 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Input, Form } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
-// import { Container } from './styles';
+import logo from '~/assets/logo.svg';
 
+const schema = Yup.object().shape({
+    email: Yup.string()
+        .email('E-mail inválido')
+        .required('Campo obrigatório'),
+    password: Yup.string().required('Campo obrigatório'),
+});
 export default function SignIn() {
-    return <h1>Login</h1>;
+    function submit(data) {
+        console.tron.log(data);
+    }
+    return (
+        <>
+            <img src={logo} alt="" />
+            <Form onSubmit={submit} schema={schema}>
+                <Input name="email" type="email" placeholder="Seu e-mail" />
+                <Input
+                    name="password"
+                    type="password"
+                    placeholder="Sua senha"
+                />
+
+                <button type="submit">Acessar</button>
+                <Link to="/register">Criar conta gratuita</Link>
+            </Form>
+        </>
+    );
 }
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,7 +1,39 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Input, Form } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
-// import { Container } from './styles';
+import logo from '~/assets/logo.svg';
+
+const schema = Yup.object().shape({
+    email: Yup.string()
+        .email('E-mail inválido')
+        .required('Campo obrigatório'),
+    password: Yup.string()
+        .min(6, 'A senha deve ter no mínimo 6 caracteres')
+        .required('Campo obrigatório'),
+    name: Yup.string().required('Campo obrigatório'),
+});
 
 export default function SignUp() {
-    return <div />;
+    function submit(data) {
+        console.tron.log(data);
+    }
+    return (
+        <>
+            <img src={logo} alt="" />
+            <Form onSubmit={submit} schema={schema}>
+                <Input name="name" type="text" placeholder="Nome completo" />
+                <Input name="email" type="email" placeholder="Seu e-mail" />
+                <Input
+                    name="password"
+                    type="password"
+                    placeholder="Sua senha"
+                />
+
+                <button type="submit">Criar conta</button>
+                <Link to="/">Já tenho conta</Link>
+            </Form>
+        </>
+    );
 }
